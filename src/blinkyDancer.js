17ae429c7e0a8ed3c5a950922a91debf74f704fa
Grant Taggart <granttaggartsf@gmail.com>
2020-07-09 07:48:29 -0700

    passed tests for dancer function

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,26 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  debugger;
+  this.$node = $('<span class="blinkyDancer"></span>');
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  //var oldStep = blinkyDancer.step
+};
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
+MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
 
-  return blinkyDancer;
+MakeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  //Animal.prototype.move.call(…)
+  //debugger;
+  MakeDancer.prototype.step.call(this);
+ // oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+
+  this.$node.toggle();
 };
