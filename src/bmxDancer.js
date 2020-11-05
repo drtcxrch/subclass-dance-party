7870e4d4a93a617a385a532352a4d21ac2cc1755
Anthony Sim <anthonyysim@gmail.com>
2020-11-05 11:49:47 -0800

    resized images

--- src/bmxDancer.js
+++ src/bmxDancer.js
@@ -8,22 +8,22 @@ var makebmxDancer = function(top, left, timeBetween) {
 makebmxDancer.prototype = Object.create(makeBlinkyDancer.prototype);
 makebmxDancer.prototype.constructor = makebmxDancer;
 makebmxDancer.prototype.prevStep = makebmxDancer.prototype.step;
-makebmxDancer.prototype.bounce = function(bouncingCount, speed) {
-  var top = 200;
-  var speedRatio = speed / top;
-  var heightRatio = top / bouncingCount;
+// makebmxDancer.prototype.bounce = function(bouncingCount, speed) {
+//   var top = 200;
+//   var speedRatio = speed / top;
+//   var heightRatio = top / bouncingCount;
 
-  for (var i = 1; i <= bouncingCount; i++) {
-    $('.BMXDancer').animate({
-      'bottom' : top
-    }, speed);
-    $('.BMXDancer').animate({
-      'bottom' : 0
-    }, speed / 2);
-    top = top - (heightRatio);
-    speed = speedRatio * top;
-  }
-};
+//   for (var i = 1; i <= bouncingCount; i++) {
+//     $('.BMXDancer').animate({
+//       'bottom' : top
+//     }, speed);
+//     $('.BMXDancer').animate({
+//       'bottom' : 0
+//     }, speed / 2);
+//     top = top - (heightRatio);
+//     speed = speedRatio * top;
+//   }
+// };
 
 
 makebmxDancer.prototype.step = function() {
@@ -35,11 +35,7 @@ makebmxDancer.prototype.step = function() {
 
   this.$node.slideUp(750);
   this.$node.slideDown(750);
-  this.bounce(10, 500);
-  // this.$node.toggle();
-  // this.$node.toggle('bounce');
-
-
+  // this.bounce(10, 500);
 
 };
 
