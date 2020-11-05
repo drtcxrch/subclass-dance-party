671cbfe759a2f13e4f3b705f2fc2ec5e94c28dd6
Anthony Sim <anthonyysim@gmail.com>
2020-11-05 12:23:26 -0800

    Complete adding css

--- src/bmxDancer.js
+++ src/bmxDancer.js
@@ -10,23 +10,6 @@ var makebmxDancer = function(top, left, timeBetween) {
 makebmxDancer.prototype = Object.create(makeBlinkyDancer.prototype);
 makebmxDancer.prototype.constructor = makebmxDancer;
 makebmxDancer.prototype.prevStep = makebmxDancer.prototype.step;
-// makebmxDancer.prototype.bounce = function(bouncingCount, speed) {
-//   var top = 200;
-//   var speedRatio = speed / top;
-//   var heightRatio = top / bouncingCount;
-
-//   for (var i = 1; i <= bouncingCount; i++) {
-//     $('.BMXDancer').animate({
-//       'bottom' : top
-//     }, speed);
-//     $('.BMXDancer').animate({
-//       'bottom' : 0
-//     }, speed / 2);
-//     top = top - (heightRatio);
-//     speed = speedRatio * top;
-//   }
-// };
-
 
 makebmxDancer.prototype.step = function() {
   var that = this;
