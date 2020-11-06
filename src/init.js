f3500cdb312b1926885eaf27d5daed0d2c3d0250
Anthony Sim <anthonyysim@gmail.com>
2020-11-06 09:56:39 -0800

    Complete dancer interactions

--- src/init.js
+++ src/init.js
@@ -65,4 +65,39 @@ $(document).ready(function() {
       }
     }
   });
+
+  // teamup function
+  $('.teamupButton').on('click', function(event) {
+    let bunnyHop = {
+      top: 100, // should increase so its not stacked
+      left: 100
+    };
+
+    let freeStyle = {
+      top: 100, // should increase so its not stacked
+      left: 1350
+    };
+
+    let blinky = {
+      top: 550,
+      left: 550
+    };
+    let { dancers } = window;
+
+    for (let i = 0; i < dancers.length; i++) {
+      if (dancers[i].$node[0].classList.contains('BlinkyDancer')) {
+        dancers[i].$node.css(blinky);
+        blinky.left += 100;
+
+      } else if (dancers[i].$node[0].classList.contains('BMXDancer')) {
+        dancers[i].$node.css(bunnyHop);
+        bunnyHop.top += 100;
+
+      } else {
+        dancers[i].$node.css(freeStyle);
+        freeStyle.top += 100;
+      }
+    }
+  });
+
 });
